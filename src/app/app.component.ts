b02d3216c50adca52ce44d532f521a61348d2ee0
johnmarwin <kakiyev95@gmail.com>
2025-09-14 18:44:49 +0300

    Add_new_changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,76 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+const name: string = 'Ahmed'
+const surname: string = 'Kakiyev'
+
+console.log(name === surname)
+
+if (name === 'Ahmed') {
+  console.log('имя без ошибок')
+} else {
+  console.log('в имени ошибка')
+}
+
+// const name = `Ahmed`;
+
+// if (name === `Ahmed`) { 
+   //console.log(`name is Ahmed`)
+// } else {
+   //console.log(`ERROR`)
+// }
+//  const func1 = (value: number) => console.log('value is:', value);
+
+
+
+// const names = ['Ahmed','Adam','Muhammad']
+
+// console.log(names[0])
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule, NgIf, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
+Pages = [1, 2, 3, 4, 5]
+newPages = this.Pages.slice().reverse();
+
+activePage = 5;
+
+isShowCatalog = true;
+
+isShowImg: boolean = true;
+
+catalog = ["Каталог", "Стройматериалы", "Инструменты", "Электрика", "Интерьер и одежда"];
+
+isUpperCase = false;
+
+toggleText() {
+  this.isUpperCase = !this.isUpperCase;
+}
+
+readonly firstheader1 = `Главная`
+
+readonly firstheader2 = `О компании`
+
+readonly firstheader3 = `Каталог`
+
+
+quantity: any;
+firstheader: any;
+increase() {
+throw new Error('Method not implemented.');
+}
+decrease() {
+throw new Error('Method not implemented.');
+}
   title = 'mentoring-first-project';
 }
+
+
